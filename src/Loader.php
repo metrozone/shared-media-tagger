c7248712dc2e4f331a9a54c2f1f84496da397d18
metrozone <41282170+metrozone@users.noreply.github.com>
2018-07-16 08:59:46 +0200

    v1.1.3 1

--- src/Loader.php
+++ src/Loader.php
@@ -25,7 +25,7 @@ class Loader
      */
     public function __construct(array $config = [])
     {
-        define('SHARED_MEDIA_TAGGER', '1.1.3');
+        define('SHARED_MEDIA_TAGGER', '1.1.3.1');
 
         ob_start('ob_gzhandler');
 
